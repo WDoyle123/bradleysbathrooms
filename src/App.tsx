bd5d6c8a4d9e3f3e2c8599b3a1c5a1d0a4f5a8f7
William Doyle <will.doyle30@gmail.com>
2025-12-02 23:32:40 +0000

    feat: scroll to hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import GalleryPage from "./pages/GalleryPage";
 import TeamPage from "./pages/TeamPage";
+import { useEffect } from "react";
 import "./App.css";
+
+function ScrollToHash() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <Router>
+      <ScrollToHash />
       <div className="overflow-x-hidden">
         <Navbar />
         <Routes>
