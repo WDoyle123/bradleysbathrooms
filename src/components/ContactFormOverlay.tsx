d4e98df458f56e291bf517667c373bc764701f25
William Doyle <will.doyle30@gmail.com>
2025-12-02 23:42:31 +0000

    feat: bigger escape button

--- src/components/ContactFormOverlay.tsx
+++ src/components/ContactFormOverlay.tsx
@@ -42,11 +42,13 @@ export default function ContactFormOverlay({
     >
       <div className="iframe-container rounded-lg relative w-11/12 md:w-3/4 h-[80vh] shadow-lg">
         <button
-          className="close-button absolute top-2 right-2 text-2xl font-bold"
+          className="btn btn-circle btn-lg absolute -top-4 -right-4 md:top-2 md:right-2 bg-error text-error-content hover:bg-error/80 shadow-xl z-10"
           onClick={onClose}
           aria-label="Close contact form"
         >
-          &times;
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
         </button>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSfyO6gHuGkORu2BPgmduoPRKaW1Wvpww_VuxuUtrRcgwqpcBQ/viewform?embedded=true"
